3913dc93bd27df16f310953ea4432d24cb93cbe6
42Wor <wwork4287@gmail.com>
2025-10-20 22:11:42 -0700

    Update model manifest: change manifest URL and add new models for enhanced functionality

--- mallow/config.py
+++ mallow/config.py
@@ -1,5 +1,3 @@
-# mallow/config.py
-
 from pathlib import Path
 
 # The base directory for all Mallow files (~/.mallow)
@@ -8,5 +6,6 @@ MALLOW_DIR = Path.home() / ".mallow"
 # The directory where models will be downloaded
 MODELS_DIR = MALLOW_DIR / "models"
 
-
-MANIFEST_URL = "https://github.com/42Wor/mallow-cli/blob/a82d37eabf2122f77c792b427d3e2899a4f60a86/mallow/models.json?raw=true"
+# The remote URL for the model manifest file
+# --- CHANGE THIS LINE ---
+MANIFEST_URL = "https://raw.githubusercontent.com/42Wor/mallow-cli/main/mallow/models.json"
